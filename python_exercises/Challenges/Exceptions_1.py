b3e235e7653583c0a1149b75e1527d44c0834de7
Narendra Parigi <naren.parigi@gmail.com>
2018-03-10 00:43:12 +0100

    deleted unwanted files

--- python_exercises/Challenges/Exceptions_1.py
+++
@@ -1,6 +0,0 @@
-try:
-    a = int(input("Tell me one number: "))
-    b = int(input("Tell me another number: "))
-    print("a/b = ", a/b)
-except:
-    print("Bug in user input.")
--- python_exercises/Challenges/fme.py
+++
@@ -1,5 +0,0 @@
-print __name__
-for i in range(10):
-   x = 0.1*i
-   print x
-   print x/(1-x*x)
